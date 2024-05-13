22c9dd79e79617b1eccfbc8100ed5b42f7ccd98d
friedmanrivka <hand1c26@mbjcomp.org.il>
2024-05-14 01:32:28 +0300

    create react side

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>Connect4Solisers</h1>
+        
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+       
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
